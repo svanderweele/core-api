202ac9ed4c87f71ce934c20688750af4f73bfb65
Simon van der Weele <vanderweelesimon@gmail.com>
2022-10-25 19:44:41 +0200

    feat: deleted extra folder

--- Core.Gaming.API/src/Core.Gaming.API/Contracts/Requests/CreateGameCategoryRequest.cs
+++
@@ -1,6 +0,0 @@
-namespace Core.Gaming.API.Contracts.Requests;
-
-public class CreateGameCategoryRequest
-{
-    public string Name { get; init; } = default!;
-}
--- Core.Gaming.API/src/Core.Gaming.API/ValidateTokenResponse.cs
+++
@@ -1,9 +0,0 @@
-namespace Core.Gaming.API;
-
-//TODO: use a shared library to share this response
-public class ValidateTokenResponse
-{
-    public string Email { get; init; } = default!;
-    public string Name { get; init; } = default!;
-    public Guid Id { get; init; } = default!;
-}
