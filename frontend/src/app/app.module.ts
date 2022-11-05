cc0c99942d8983a624d428b14869b138fb6e51c5
Simon van der Weele <vanderweelesimon@gmail.com>
2022-11-05 15:29:14 +0100

    feat: add translations

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,9 +7,16 @@ import { ToastrModule } from 'ngx-toastr';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { AuthGuard } from './guards/AuthGuard';
 import { NotLoggedInGuard } from './guards/NotLoggedInGuard';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { ReactiveFormsModule } from '@angular/forms';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [AppComponent, NavbarComponent],
@@ -19,6 +26,14 @@ import { NotLoggedInGuard } from './guards/NotLoggedInGuard';
     HttpClientModule,
     BrowserAnimationsModule,
     ToastrModule.forRoot(),
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
+    ReactiveFormsModule,
   ],
   providers: [AuthGuard, NotLoggedInGuard],
   bootstrap: [AppComponent],
