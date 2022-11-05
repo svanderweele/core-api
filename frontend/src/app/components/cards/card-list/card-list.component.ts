b76be5ebc3e06df81f28760bea78d745a57417b9
Simon van der Weele <vanderweelesimon@gmail.com>
2022-11-05 14:38:05 +0100

    feat: improve casino page

--- frontend/src/app/components/cards/card-list/card-list.component.ts
+++ frontend/src/app/components/cards/card-list/card-list.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { PaginationService } from 'src/app/globals';
 import { Game } from '../game.models';
+import { DeviceDetectorService } from 'ngx-device-detector';
 
 @UntilDestroy()
 @Component({
@@ -10,16 +11,24 @@ import { Game } from '../game.models';
   styleUrls: ['./card-list.component.css'],
 })
 export class CardListComponent {
+  itemsPerPage = 10;
+
   lastItemKey?: string = undefined;
   isLoading = false;
   isLastItemReached = false;
   games: Game[] = [];
-  totalItems = 0;
 
   /**
    *
    */
-  constructor(private paginationService: PaginationService) {
+  constructor(
+    private paginationService: PaginationService,
+    private deviceDetectorService: DeviceDetectorService
+  ) {
+    if (this.deviceDetectorService.isMobile()) {
+      this.itemsPerPage = 3;
+    }
+
     this.loadGames();
   }
 
@@ -35,12 +44,12 @@ export class CardListComponent {
     this.isLoading = true;
 
     this.paginationService
-      .getAll({ limit: 3, startKey: this.lastItemKey })
+      .getAll({ limit: this.itemsPerPage, startKey: this.lastItemKey })
       .pipe(untilDestroyed(this))
       .subscribe((val) => {
         const freshGames = val.getItems() as Game[];
 
-        this.isLastItemReached = freshGames.length < 3;
+        this.isLastItemReached = freshGames.length < this.itemsPerPage;
         this.games = [...this.games, ...freshGames];
         this.lastItemKey = val.lastItemKey;
         this.isLoading = false;
